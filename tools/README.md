227d77faf52ec0382e4d83ddc4478c1c291677fb
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2023-08-21 23:03:08 +0800

    feat: add csv2sqlite tool

---
+++ tools/README.md
@@ -0,0 +1,22 @@
+# CSV to SQLite converter
+
+A simple Python script to convert a CSV file into an SQLite database.
+
+Basic usage:
+
+```bash
+python csv-to-sqlite.py <mydata.csv>
+```
+
+This will create an SQLite file named mydata.db in the same directory.
+
+## SQLite Viewer
+
+To confirm that the SQLite file has been successfully created, you can use a
+handy online tool [SQLite Viewer](https://inloop.github.io/sqlite-viewer/) to
+display its contents.
+
+## Sample Data
+
+The `examples` folder contains temperature and humidity data that shows a
+sample output from the script.
---
+++ tools/csv-to-sqlite.py
@@ -0,0 +1,31 @@
+# simple script to convert a csv file into an sqlite database
+
+import os
+import pandas
+import sqlite3
+import sys
+
+def main():
+    # get the filename without extension
+    filename = str(os.path.splitext(sys.argv[1])[0])
+
+    # add appropriate extensions
+    csvfile = filename + ".csv"
+    dbfile = filename + ".db"
+
+    try:
+        dataframe = pandas.read_csv(csvfile)
+    except FileNotFoundError:
+        print(f"error: file {csvfile} not found")
+        exit(1)
+    
+    conn = sqlite3.connect(dbfile)
+    dataframe.to_sql(filename, conn, if_exists='replace', index=False)
+    conn.close()
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print(f"usage: python {os.path.basename(__file__)} <csv file>")
+        exit(0)
+
+    main()
