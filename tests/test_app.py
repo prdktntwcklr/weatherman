849193df58f8b659b37402bb77a3af03fcec3d06
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2023-08-23 23:28:36 +0800

    feat: display data in table

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_hello():
     response  = app.test_client().get('/')
 
     assert response.status == '200 OK'
-    assert response.data == b'Ciao mondo!'
+    assert response.data == b'Hello World!'
