04c1b88b4df6a3813bcc7efe6a85155c207a60c6
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2024-12-16 22:13:26 +0800

    docs: add comment about flask factory (#27)

--- weatherman/__init__.py
+++ weatherman/__init__.py
@@ -1,22 +1,25 @@
+"""main Flask application"""
+
 from config import config
 from datetime import datetime
 from flask import Flask, render_template
+from . import db
 
 
-def create_app(config_name='default'):
+# create_app factory is autodetected by Flask
+def create_app(config_name="default"):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     print(f"Database file used is: {app.config.get('DATABASE')}")
 
-    from . import db
     db.init_app(app)
 
     with app.app_context():
         db.init_db()
 
-    @app.route('/')
+    @app.route("/")
     def index():
         rows = db.get_rows()
 
@@ -28,9 +31,9 @@ def create_app(config_name='default'):
 
         return render_template("data.html", data=rows)
 
-    @app.template_filter('strftime')
-    def pretty_date(dateString, fmt='%Y-%m-%dT%H:%M+00:00'):
-        dt = datetime.strptime(dateString, fmt)
-        return dt.strftime('%Y-%m-%d %H:%M')
+    @app.template_filter("strftime")
+    def pretty_date(date_string, fmt="%Y-%m-%dT%H:%M+00:00"):
+        dt = datetime.strptime(date_string, fmt)
+        return dt.strftime("%Y-%m-%d %H:%M")
 
     return app
